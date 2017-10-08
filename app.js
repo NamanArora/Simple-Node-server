d6484c2ae7d04398168938263e831248e89e7cef
NamanArora <naman.arora11@gmail.com>
2017-10-08 22:08:35 +0530

    Ip added

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 var http = require('http');
 
-listener =(req,res) =>{
-   
-}
+var ip = '192.168.0.106';
 
 var server = http.createServer((req,res)=>{
     res.writeHead(200,{
         'Content-Type': 'text/plain'
     });
-    res.end('hello nigga');
+    res.end('hello gangsta');
 });
-server.listen(3000,'localhost');
+server.listen(3000,ip || 'localhost');
