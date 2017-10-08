47e9b398e5d883393712829e477931514663608e
NamanArora <naman.arora11@gmail.com>
2017-10-08 22:28:52 +0530

    File reading capabilities

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var http = require('http');
+var fs = require('fs');
 
 var ip = '192.168.0.106';
 
@@ -6,6 +7,7 @@ var server = http.createServer((req,res)=>{
     res.writeHead(200,{
         'Content-Type': 'text/plain'
     });
-    res.end('hello gangsta');
+    var read = fs.createReadStream(__dirname + '/sample.txt' , 'utf-8');
+    read.pipe(res);
 });
 server.listen(3000,ip || 'localhost');
